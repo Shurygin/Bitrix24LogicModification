939ebee3000f1f692221b920558a10bdc3db21d8
Aleksey <magnito12@yandex.ru>
2019-06-03 14:23:47 +0300

    Update Entity.php

--- local/php_interface/Entity.php
+++ local/php_interface/Entity.php
@@ -6,26 +6,22 @@ namespace BitEntity;
 
 class Entity
 {
-    public static function log($param1=[],$param2=[],$param3=[])
+    public static function log($a='no params')
     {
         $date = date('m-d');
         if (!is_dir(__DIR__.'/'.$date))
         {
             mkdir(__DIR__.'/'.$date);
         }
-        file_put_contents(__DIR__.'/'.$date.'/log.txt',print_r([$param1,$param2,$param3],1),FILE_APPEND);
+        $params=func_get_args();
+        file_put_contents(__DIR__.'/'.$date.'/log.txt',print_r($params,1),FILE_APPEND);
     }
 
-    public static function dd($param1=[],$param2=[],$param3=[])
+    public static function dd($a='no params')
     {
+        $params=func_get_args();
         echo "<pre>";
-        echo print_r($param1, 1);
-        echo "</pre>";
-        echo "<pre>";
-        echo print_r($param2, 1);
-        echo "</pre>";
-        echo "<pre>";
-        echo print_r($param3, 1);
+        echo print_r($params, 1);
         echo "</pre>";
         die;
     }
